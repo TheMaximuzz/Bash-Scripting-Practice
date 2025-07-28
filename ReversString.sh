bbafff9caede3f25f16b43a0fec6a3ba77a89335
TheMaximuzz <maks.natorin@gmail.com>
2025-07-28 14:05:02 +0500

    Create ReverseString.sh
    Reversing strings (reading them from right to left, rather than from left to right) is a surprisingly common task in programming.
    
    For example, in bioinformatics, reversing the sequence of DNA or RNA strings is often important for various analyses, such as finding complementary strands or identifying palindromic sequences that have biological significance.

---
+++ ReversString.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+#This script make reversed strings
+
+usage()
+{
+echo "USAGE: ./ReverseScript.sh S, where S is string. Reverse string"
+}
+
+[[ $# -ne 1 ]] && usage && exit 1
+
+res=""
+
+reverse() {
+str=$1
+len=`echo str | wc -m`
+echo "Total len: $len"
+for (( i=len; i>=0; i-- )); do
+        res+="${str:$i:1}"
+done
